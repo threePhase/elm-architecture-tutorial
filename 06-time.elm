562f10056fee44b01e406cc5f1544682340d9b62
Michael Snider <snidemic@gmail.com>
2016-08-17 21:00:18 -0700

    Wire up Start/Stop button to Power message

--- 06-time.elm
+++ 06-time.elm
@@ -2,6 +2,7 @@ import Html exposing (Html)
 import Html.App as App
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Svg.Events exposing (onClick)
 import Time exposing (Time, second)
 
 
@@ -16,25 +17,36 @@ main =
 
 -- MODEL
 
-type alias Model = Time
+type alias Model =
+  { power : Bool
+  , time : Time
+  }
 
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  let model =
+    { power = True
+    , time = 0
+    }
+    in
+  (model, Cmd.none)
 
 
 -- UPDATE
 
 type Msg
   = Tick Time
+  | Power
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ({ model | time = newTime }, Cmd.none)
+    Power ->
+      ({ model | power = (not model.power) }, Cmd.none)
 
 
 -- SUBSCRIPTIONS
@@ -51,11 +63,12 @@ view model =
     svg [ viewBox "0 0 100 150", width "300px" ]
       [ drawClock model, drawButton ]
 
-drawClock : Model -> Svg msg
+drawClock : Model -> Svg Msg
 drawClock model =
   let
+    time = if model.power then model.time else 0
     angle =
-      turns (Time.inMinutes model)
+      turns (Time.inMinutes time)
 
     handX =
       toString (50 + 40 * cos angle)
@@ -68,9 +81,11 @@ drawClock model =
       , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963"] []
       ]
 
-drawButton : Svg msg
+drawButton : Svg Msg
 drawButton =
-  g [ Svg.Attributes.cursor "pointer" ]
+  g [ Svg.Attributes.cursor "pointer"
+    , onClick Power
+    ]
     [
      rect [ fill "none"
           , stroke "black"
