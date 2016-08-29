1e14bec1607a72e1a3ed5208471e0e8862917f39
Michael Snider <snidemic@gmail.com>
2016-08-28 22:18:16 -0700

    Improve template file formatting
    - Organize sections to have a sense of ordering
    - Add type annotation to main
    - Improve formatting

--- template.elm
+++ template.elm
@@ -2,14 +2,27 @@ import Html exposing (..)
 import Html.App as Html
 
 
+main : Program Never
 main =
-  Html.program { init = init, update = update, subscriptions = subscriptions, view = view}
+  Html.program { init = init
+               , update = update
+               , subscriptions = subscriptions
+               , view = view
+               }
+
 
 -- MODEL
 
 type alias Model =
   { ...
   }
+
+
+-- INIT
+
+init : (Model, Cmd Msg)
+init =
+  ...
 
 
 -- UPDATE
@@ -21,22 +34,14 @@ update msg model =
   ...
 
 
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  ...
-
-
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
 
 
+-- VIEW
 
--- INIT
-
-init : (Model, Cmd Msg)
-init =
+view : Model -> Html Msg
+view model =
   ...
